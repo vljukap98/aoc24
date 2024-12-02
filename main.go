0ad25bf6e02a63bbe138c27b05f1d55015edc608
ljakovic <luka@arhivpro.hr>
2024-12-02 17:05:35 +0100

    day2 init

--- main.go
+++ main.go
@@ -1,8 +1,38 @@
 package main
 
-import "aoc24/day1"
+import (
+	"aoc24/day1"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func getFireSignalsChannel() chan os.Signal {
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c,
+		// https://www.gnu.org/software/libc/manual/html_node/Termination-Signals.html
+		syscall.SIGTERM, // "the normal way to politely ask a program to terminate"
+		syscall.SIGINT,  // Ctrl+C
+		syscall.SIGQUIT, // Ctrl-\
+		syscall.SIGKILL, // "always fatal", "SIGKILL and SIGSTOP may not be caught by a program"
+		syscall.SIGHUP,  // "terminal is disconnected"
+	)
+	return c
+
+}
+
+func exit() {
+	syscall.Kill(syscall.Getpid(), syscall.SIGTERM)
+	fmt.Println("Exiting!")
+}
 
 func main() {
-	day1.Day1Part1()
+	exitChan := getFireSignalsChannel()
+
 	day1.Day1Part2()
+	// time.Sleep(10 * time.Second)
+	<-exitChan
+	return
 }
