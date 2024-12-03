1d3f43d80f0eee456dd4347e488b3e1e1d40de23
Luka Jakovic <ljakovi98@gmail.com>
2024-12-03 22:33:23 +0100

    day2 repair

--- day2/day2.go
+++ day2/day2.go
@@ -49,10 +49,12 @@ func Day2Part2() {
 
 	for _, row := range inputRows {
 		rowElems := util.ArrStrToI(strings.Split(row, " "))
-
-		if isSafe2(rowElems, 0) {
+		fmt.Println(rowElems)
+		if isSafe2(rowElems) || canBeSafeByRemovingOne(rowElems) {
 			safeLevels += 1
+			fmt.Println("safe")
 		} else {
+			fmt.Println("unsafe")
 			continue
 		}
 	}
@@ -97,61 +99,36 @@ func isSafe(numbers []int) bool {
 	return true
 }
 
-func isSafe2(numbers []int, iter int) bool {
-	badCount := 0
-	var isIncreasing1 bool
-	var isIncreasing2 bool
-
-	for i := 0; i < len(numbers); i++ {
-		if numbers[i] == numbers[i+1] {
-			if badCount > 0 {
-				return false
-			} else {
-				badCount++
-				i--
-				//remove item
-				continue
-			}
-		}
-		if i > 0 {
-			if numbers[i] < numbers[i+1] {
-				isIncreasing2 = true
-			} else {
-				isIncreasing2 = false
-			}
-
-			if isIncreasing1 != isIncreasing2 {
-				if badCount > 0 {
-					return false
-				} else {
-					badCount++
-					i--
-					//remove item
-					continue
-				}
-			}
-		} else {
-			if numbers[i] < numbers[i+1] {
-				isIncreasing1 = true
-			} else {
-				isIncreasing1 = false
-			}
-		}
-
-		distanceInt := numbers[i] - numbers[i+1]
-		distance := math.Abs(float64(distanceInt))
-		if distance < 1 || distance > 3 {
-			if badCount > 0 {
-				return false
-			} else {
-				badCount++
-				i--
-				//remove item
-				continue
-			}
-		}
-
+func isSafe2(report []int) bool {
+	if len(report) < 2 {
+		return true
 	}
+	increasing := true
+	decreasing := true
 
-	return badCount == 0
+	for i := 1; i < len(report); i++ {
+		diff := report[i] - report[i-1]
+		if diff < 1 || diff > 3 {
+			return false
+		}
+		if diff > 0 {
+			decreasing = false
+		} else if diff < 0 {
+			increasing = false
+		}
+	}
+	return increasing || decreasing
+}
+
+// Checks if the report can be made safe by removing a single element
+func canBeSafeByRemovingOne(report []int) bool {
+	for i := range report {
+		newReport := make([]int, 0, len(report)-1)
+		newReport = append(newReport, report[:i]...)
+		newReport = append(newReport, report[i+1:]...)
+		if isSafe(newReport) {
+			return true
+		}
+	}
+	return false
 }
