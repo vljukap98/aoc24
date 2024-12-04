c789ccf391aa5d3cf615277d985fe26ee7c281ca
Luka Jakovic <ljakovi98@gmail.com>
2024-12-04 19:58:27 +0100

    day4

--- day4/day4.go
+++ day4/day4.go
@@ -2,62 +2,92 @@ package day4
 
 import (
 	"aoc24/util"
+	"fmt"
 	"strings"
 )
 
 func Day4() {
 	lines, _ := util.ReadLines("./day4/day4-input.txt")
-	var matrix [][]int
+	var matrix [][]string
 	for _, line := range lines {
-		lineChars := strings.Split(line, "\"")
-		matrix = append(matrix, util.ArrStrToI(lineChars))
+		lineChars := strings.Split(line, "")
+		matrix = append(matrix, (lineChars))
 	}
 
 	part1(matrix)
+	part2(matrix)
 }
 
-func part1(mat [][]int) {
-	xmasCount := 0
-	for x, y := 0, 0; x < len(mat[0]); x, y = x+1, y+1 {
-		xmasCount += checkHorizontalLeft()
-		xmasCount += checkHorizontalRight()
-		xmasCount += checkVerticalDown()
-		xmasCount += checkVerticalUp()
-		xmasCount += checkDiagonalLeftDown()
-		xmasCount += checkDiagonalLeftUp()
-		xmasCount += checkDiagonalRightDown()
-		xmasCount += checkDiagonalRightUp()
+func part1(mat [][]string) {
+	count := 0
+
+	for y := 3; y < len(mat)-2; y++ {
+		for x := 3; x < len(mat[y])-2; x++ {
+			//right
+			if mat[y][x] == "X" && mat[y][x+1] == "M" && mat[y][x+2] == "A" && mat[y][x+3] == "S" {
+				count++
+			}
+			//left
+			if mat[y][x] == "X" && mat[y][x-1] == "M" && mat[y][x-2] == "A" && mat[y][x-3] == "S" {
+				count++
+			}
+			//up
+			if mat[y][x] == "X" && mat[y-1][x] == "M" && mat[y-2][x] == "A" && mat[y-3][x] == "S" {
+				count++
+			}
+			//down
+			if mat[y][x] == "X" && mat[y+1][x] == "M" && mat[y+2][x] == "A" && mat[y+3][x] == "S" {
+				count++
+			}
+			//down right
+			if mat[y][x] == "X" && mat[y+1][x+1] == "M" && mat[y+2][x+2] == "A" && mat[y+3][x+3] == "S" {
+				count++
+			}
+			//up right
+			if mat[y][x] == "X" && mat[y-1][x+1] == "M" && mat[y-2][x+2] == "A" && mat[y-3][x+3] == "S" {
+				count++
+			}
+			//down left
+			if mat[y][x] == "X" && mat[y+1][x-1] == "M" && mat[y+2][x-2] == "A" && mat[y+3][x-3] == "S" {
+				count++
+			}
+			//up left
+			if mat[y][x] == "X" && mat[y-1][x-1] == "M" && mat[y-2][x-2] == "A" && mat[y-3][x-3] == "S" {
+				count++
+			}
+		}
 	}
+
+	fmt.Println(count)
 }
 
-func checkHorizontalLeft() int {
-	panic("unimplemented")
-}
+func part2(mat [][]string) {
+	count := 0
 
-func checkHorizontalRight() int {
-	panic("unimplemented")
-}
+	for y := 3; y < len(mat)-2; y++ {
+		for x := 3; x < len(mat[y])-2; x++ {
+			if mat[y][x] == "A" {
 
-func checkVerticalDown() int {
-	panic("unimplemented")
-}
+				// up left M
+				if mat[y-1][x-1] == "M" && mat[y+1][x+1] == "S" {
+					// down left MAS
+					downLeftMAS := mat[y+1][x-1] == "M" && mat[y-1][x+1] == "S"
+					downLeftSAM := mat[y+1][x-1] == "S" && mat[y-1][x+1] == "M"
+					if downLeftMAS || downLeftSAM {
+						count++
+					}
+				}
+				// up left S
+				if mat[y-1][x-1] == "S" && mat[y+1][x+1] == "M" {
+					downLeftMAS := mat[y+1][x-1] == "M" && mat[y-1][x+1] == "S"
+					downLeftSAM := mat[y+1][x-1] == "S" && mat[y-1][x+1] == "M"
+					if downLeftMAS || downLeftSAM {
+						count++
+					}
+				}
+			}
+		}
+	}
 
-func checkVerticalUp() int {
-	panic("unimplemented")
-}
-
-func checkDiagonalLeftDown() int {
-	panic("unimplemented")
-}
-
-func checkDiagonalLeftUp() int {
-	panic("unimplemented")
-}
-
-func checkDiagonalRightDown() int {
-	panic("unimplemented")
-}
-
-func checkDiagonalRightUp() int {
-	panic("unimplemented")
+	fmt.Println(count)
 }
--- main.go
+++ main.go
@@ -1,15 +1,8 @@
 package main
 
-import (
-	"aoc24/day1"
-	"aoc24/day2"
-	"aoc24/day3"
-	"aoc24/day4"
-)
-
 func main() {
-	day1.Day1()
-	day2.Day2()
-	day3.Day3()
-	day4.Day4()
+	// day1.Day1()
+	// day2.Day2()
+	// day3.Day3()
+	// day4.Day4()
 }
