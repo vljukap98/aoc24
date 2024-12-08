bc9d966d98833b73adaf93a9e206ab961cd20e6e
Luka Jakovic <ljakovi98@gmail.com>
2024-12-08 21:36:49 +0100

    da6part2 garbonzo

--- day6/day6.go
+++ day6/day6.go
@@ -25,7 +25,7 @@ type coordinate struct {
 }
 
 func Day6() {
-	input, _ := util.ReadLines("./day6/day6-input.txt")
+	input, _ := util.ReadLines("./day6/day6-input-test.txt")
 
 	coordinates := make(map[coordinate]bool)
 	guard := guardState{}
@@ -48,10 +48,10 @@ func Day6() {
 }
 
 func part1(mat [][]string, guard *guardState, coordinates map[coordinate]bool) {
-	for guard.CurrCoord.x != -1 && guard.CurrCoord.y != -1 {
-		coordinates[guard.CurrCoord] = true
-		moveGuard(mat, guard)
-	}
+	// for guard.CurrCoord.x != -1 && guard.CurrCoord.y != -1 {
+	// 	coordinates[guard.CurrCoord] = true
+	// 	moveGuard(mat, guard)
+	// }
 
 	visits := 0
 	for _, coords := range coordinates {
@@ -108,17 +108,83 @@ func moveGuard(mat [][]string, guard *guardState) {
 }
 
 func part2(mat [][]string) {
-	for y := 0; y < len(mat)-1; y++ {
-		for x := 0; x < len(mat[y])-1; x++ {
-			check(x, y, mat, directionRight)
+	count := 0
+	for y := 1; y < len(mat)-1; y++ {
+		for x := 1; x < len(mat[y])-1; x++ {
+			if mat[y][x] == "#" && (checkFirstRight(x, y, mat, directionRight, x) ||
+				checkFirstUp(x, y, mat, directionUp) ||
+				checkFirstLeft(x, y, mat, directionLeft)) {
+				count++
+			}
 		}
 	}
+
+	fmt.Println(count)
 }
 
-func check(x int, y int, mat [][]string, direction int) {
-	//TODO recursively check whether '#' matches depending on direction
+func checkFirstRight(x int, y int, mat [][]string, direction int, firstX int) bool {
+	if direction == directionRight {
+		for i := x + 1; i < len(mat[y+1]); i++ {
+			if mat[y+1][i] == "#" {
+				return checkFirstRight(i, y, mat, directionDown, x)
+			}
+		}
+	}
+
+	if direction == directionDown {
+		for i := y + 1; i < len(mat)-1; i++ {
+			if mat[i][x-1] == "#" {
+				return checkFirstRight(x, i, mat, directionLeft, x)
+			}
+		}
+	}
+
+	//TODO: check if from current x to first x are not obstacles
+	// this means I can put an obstacle to first x+-1 to put the guard on the loop
+
+	return false
+}
+
+func checkFirstUp(x int, y int, mat [][]string, direction int) bool {
+	if direction == directionUp {
+		for i := y - 1; i > 0; i-- {
+			if mat[i][x+1] == "#" {
+				return checkFirstUp(x, i, mat, directionRight)
+			}
+		}
+	}
 
 	if direction == directionRight {
-
+		for i := x + 1; i < len(mat[y+1]); i++ {
+			if mat[y+1][i] == "#" {
+				return checkFirstUp(i, y, mat, directionDown)
+			}
+		}
 	}
+
+	//TODO: check if from current y to first one are not obstacles
+
+	return false
+}
+
+func checkFirstLeft(x int, y int, mat [][]string, direction int) bool {
+	if direction == directionLeft {
+		for i := x - 1; i > 0; i++ {
+			if mat[y-1][i] == "#" { // check for first row
+				return checkFirstLeft(i, y, mat, directionUp)
+			}
+		}
+	}
+
+	if direction == directionUp {
+		for i := y - 1; i > 0; i-- {
+			if mat[i][x+1] == "#" {
+				return checkFirstUp(x, i, mat, directionRight)
+			}
+		}
+	}
+
+	//TODO: check if from current x there are no obstacles
+
+	return false
 }
